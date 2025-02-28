efde61408885fe31ba25eabb11ec27f489ff8c44
Tanyakalra20 <tanyakalra61@gmail.com>
2025-02-28 22:32:26 +0530

    Initial commit - Parallax-effect-project

---
+++ script.js
@@ -0,0 +1,11 @@
+var typeData = new Typed(".define",{
+    strings: [
+        "DVENTURE"
+       ],
+       loop:true,
+       typeSpeed: 100,
+       backspeed: 80,
+       backDelay: 1000,
+    });
+    
+    
